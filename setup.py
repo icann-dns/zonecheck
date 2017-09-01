5c43520a02737f8ec46cce2d69244b2893769603
John Bond <github@johnbond.org>
2017-09-01 13:03:20 +0100

    make soa issues none fatal

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ class ScriptInstaller(install):
         super(ScriptInstaller, self).run()
 
 setup(name='zonecheck',
-      version='1.0.17',
+      version='1.0.18',
       description='Librarys to check zones configuered on a server are working',
       author='John Bond',
       author_email='pypi@johnbond.org',
